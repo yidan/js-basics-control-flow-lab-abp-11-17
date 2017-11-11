172d76b65dba9fdf29d4cccdcc8bbac57500b831
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 17:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ function scuberGreetingForFeet(ride){
     return 'This one is on me!'
   } else if (ride > 2500){
     return "No can do."
-    
+
   } else if (ride > 2000){
     return 'I will gladly take your thirty bucks.'
   }
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.':'No go.'
 }
