8f907000ed9f67974366793062022925b0dc452f
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 17:26:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function scuberGreetingForFeet(ride){
 function ternaryCheckCity(city){
   return city === 'NYC'? 'Ok, sounds good.':'No go.'
 }
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+    return 'Bye.'
+      
+  }
+}
