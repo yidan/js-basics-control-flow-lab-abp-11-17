edaf624593aa426a17e302abd21e37701d66bc15
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 17:26:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride<400){
+    return 'This one is on me!'
+  } else if (ride > 2500){
+    return "No can do."
+
+  } else if (ride > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.':'No go.'
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+
+  }
+}
