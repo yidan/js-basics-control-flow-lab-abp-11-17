92694bd97f7fbbf8f8148f89ae8848de2ffca4f5
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 17:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride){
+  if (ride<400){
+    return 'This one is on me!'
+  } else if (ride > 2500){
+    return "No can do."
+    
+  } else if (ride > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+}
